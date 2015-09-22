fae494aa519e362011820891bb9ef8433911dff6
Qi He <qhe@heyook.com>
2015-09-22 00:13:00 -0700

    aliyun oss file upload setup

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 /* jshint node: true */
 'use strict';
 
+var AliOSSAdapter = require('./lib/alioss');
+
 module.exports = {
-  name: 'ember-deploy-alioss'
+  name: 'ember-deploy-alioss',
+
+  type: 'ember-deploy-addon',
+
+  adapters: {
+    assets: {
+      'alioss': AliOSSAdapter
+    }
+  }
 };
